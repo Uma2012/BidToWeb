fd69fc289c6d20e7093e71284d9d192232627553
uma Prithi <umaprithi@outlook.com>
2021-06-20 09:00:23 +0200

    Create product controller to get all products

---
+++ ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAllProducts();
+    }
+}
